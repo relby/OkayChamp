2dac26594b14d178500edef6daeaa39a08ea4b4e
relby <kudinov.nikita@gmail.com>
2022-05-27 18:22:25 +0300

    Refactor avatar command

--- src/commands/user/avatar.ts
+++ src/commands/user/avatar.ts
@@ -1,20 +1,33 @@
-import { User } from "discord.js";
+import { MessageEmbed, User } from "discord.js";
 import { Command } from "../../classes/Command";
 
 export default new Command({
   name: "avatar",
-  description: "avatar",
+  description: "Displays your avatar or someone else's avatar",
   options: [
     {
       name: "user",
-      description: "user",
+      description: "The user to get avatar for",
       type: "USER",
       required: false,
-    }
+    },
   ],
   run: ({ interaction }) => {
     const user = (interaction.options.getUser("user") ?? interaction.user) as User;
-    const avatar = user.avatarURL() ?? `${user.username} doesn't have an avatar`;
-    interaction.followUp(avatar);
-  }
+    const avatar = user.displayAvatarURL({ dynamic: true, size: 1024 });
+    const embed = new MessageEmbed()
+      .setColor("RANDOM")
+      .setTitle("Avatar link")
+      .setURL(avatar)
+      .setAuthor({
+        name: `${user.username}#${user.discriminator}`,
+        iconURL: avatar,
+      })
+      .setImage(avatar)
+      .setFooter({
+        text: `Requested by ${interaction.user.username}#${interaction.user.discriminator}`,
+        iconURL: interaction.user.displayAvatarURL(),
+      });
+    interaction.followUp({ embeds: [embed] });
+  },
 });
