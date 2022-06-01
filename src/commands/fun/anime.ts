88c4d1f8680e22c5c00c4b87be23e5710462749b
relby <kudinov.nikita@gmail.com>
2022-06-02 01:29:10 +0300

    Fix extention type of picture for anime command

--- src/commands/fun/anime.ts
+++ src/commands/fun/anime.ts
@@ -45,7 +45,8 @@ export default new Command({
         responseType: "arraybuffer"
       });
 
-      const extention = response.headers['content-type'].slice(-3);
+      const extention = response.headers['content-type'].split('/').at(-1);
+      console.log(extention);
       const imageBuffer = Buffer.from(response.data);
       const isSpoiler = !(interaction.channel as TextChannel).nsfw && type === "nsfw";
       return interaction.followUp({files: [{name: fileName(endpoint, isSpoiler, extention), attachment: imageBuffer}]});
