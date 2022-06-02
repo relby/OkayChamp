2569b86dcf552e19280ebbc7599eb54f8e3ebe28
relby <kudinov.nikita@gmail.com>
2022-06-02 14:46:11 +0300

    Change find command to edit message every time it finds a match

--- src/commands/info/find.ts
+++ src/commands/info/find.ts
@@ -34,34 +34,37 @@ export default new Command({
     const channel = args.getChannel("channel", true) as TextChannel;
 
     const map: Collection<Snowflake, UserWithMessagesInChannel> = new Collection();
-    let msg_ptr = await channel.messages
+    let msgPtr = await channel.messages
       .fetch({ limit: 1 })
       .then(messagePage => (messagePage.size === 1 ? messagePage.at(0) : null));
 
-    while (msg_ptr) {
+    while (msgPtr) {
       await channel.messages
-        .fetch({ limit: 100, before: msg_ptr.id })
+        .fetch({ limit: 100, before: msgPtr.id })
         .then(messagePage => {
-
           messagePage.forEach(msg => {
             const regex = new RegExp("(\\P{L}|^)" + `(${word})` + "(\\P{L}|$)", "ium");
             if (msg.content.match(regex) == null) return;
 
             const id = msg.author.id;
-            const user_with_msgs = map.has(id) ? map.get(id)! : Object.assign({ messages: <Message[]>[], channel: channel }, msg.author);
+            const userWithMsgs = map.has(id) ? map.get(id)! : Object.assign({ messages: <Message[]>[], channel: channel }, msg.author);
 
-            user_with_msgs.messages.push(msg);
-            map.set(id, Object.assign(user_with_msgs, msg.author));
+            userWithMsgs.messages.push(msg);
+            map.set(id, Object.assign(userWithMsgs, msg.author));
+            const out: string[] = [];
+            map.forEach(({ username, discriminator, messages }) => {
+              out.push(`${username}#${discriminator} ${messages.length}`);
+            });
+            if (out.length) {
+              interaction.editReply(`<#${channel.id}>: \n` + out.join("\n"));
+            }
           });
           // Update our message pointer to be last message in page of messages
-          msg_ptr = 0 < messagePage.size ? messagePage.at(messagePage.size - 1) : null;
+          msgPtr = 0 < messagePage.size ? messagePage.at(messagePage.size - 1) : null;
         });
     }
-    const out: string[] = [];
-    // TODO: Sort the output
-    map.forEach(({ username, discriminator, messages }) => {
-      out.push(`${username}#${discriminator} ${messages.length}`);
-    });
-    return interaction.followUp(`<#${channel.id}>: \n` + (out.length ? out.join("\n") : "No messages in this channel found"));
+    if (!interaction.replied) {
+      interaction.editReply(`No messages found in <#${channel.id}>`);
+    }
   }
 });
