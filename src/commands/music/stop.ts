685135ad86be0175916255ef9982b9faf45a8e12
relby <kudinov.nikita@gmail.com>
2022-06-13 22:48:48 +0300

    finally add stop command

--- src/commands/music/stop.ts
+++ src/commands/music/stop.ts
@@ -1,3 +1,4 @@
+import { player } from "../.."
 import { Command } from "../../classes/Command";
 
 // TODO: Add option to select multiple tracks
@@ -6,6 +7,9 @@ export default new Command({
   name: "stop",
   description: "stop",
   run: async ({ interaction }) => {
-    return await interaction.followUp("test");
+    const queue = player.getQueue(interaction.guildId);
+    if (!queue || !queue.playing) return await interaction.followUp("No music is being played!");
+    queue.destroy();
+    return await interaction.followUp("Stopped the player!");
   },
 });
