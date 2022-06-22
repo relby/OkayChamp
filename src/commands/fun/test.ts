044a021e5960ceaa795d003925812343a1461b89
relby <kudinov.nikita@gmail.com>
2022-06-22 17:52:33 +0300

    help command

--- src/commands/fun/test.ts
+++ src/commands/fun/test.ts
@@ -3,16 +3,9 @@ import { Command } from "../../classes/Command";
 
 export default new Command({
   type: "CHAT_INPUT",
-  name: "test",
-  description: "test",
-  run: ({ client, interaction }) => {
-    client.guilds.cache.forEach(async (guild) => {
-      if (guild.id === "702799408466624586") {
-        const channel = await guild.channels.fetch("702799409238376472") as TextChannel;
-        await channel.send("https://stock-images.0o.si/?p#/old-man-tries-to-climb-ladder-falls-down-and-eats-cookie-2298498123%5C");
-        (await channel.messages.fetch({ limit:10 })).forEach(message => console.log(message.content));
-      }
-    });
-    return interaction.followUp("Something went wrong");
+  name: "help",
+  description: "help",
+  run: ({ interaction }) => {
+    return interaction.followUp("https://stock-images.0o.si/?p#/old-man-tries-to-climb-ladder-falls-down-and-eats-cookie-2298498123%5C");
   }
 })
