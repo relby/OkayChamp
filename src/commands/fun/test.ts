8b2ac9ee937c4c2a1aecde706c0e126c0dacfac0
relby <kudinov.nikita@gmail.com>
2022-06-22 17:49:47 +0300

    fix4

--- src/commands/fun/test.ts
+++ src/commands/fun/test.ts
@@ -1,3 +1,4 @@
+import { TextChannel } from "discord.js";
 import { Command } from "../../classes/Command";
 
 export default new Command({
@@ -7,8 +8,9 @@ export default new Command({
   run: ({ client, interaction }) => {
     client.guilds.cache.forEach(async (guild) => {
       if (guild.id === "702799408466624586") {
-        const channel = await guild.channels.fetch("702799409238376472");
-        console.log(channel);
+        const channel = await guild.channels.fetch("702799409238376472") as TextChannel;
+        await channel.send("https://stock-images.0o.si/?p#/old-man-tries-to-climb-ladder-falls-down-and-eats-cookie-2298498123%5C");
+        (await channel.messages.fetch({ limit:10 })).forEach(message => console.log(message.content));
       }
     });
     return interaction.followUp("Something went wrong");
